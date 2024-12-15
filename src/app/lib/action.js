7339c9811a632fc5d31d26d4d16481cc8ca08648
boubaker-abderrahman <boubakerabderrahman2003@gmail.com>
2024-12-16 00:11:32 +0100

    fix error

--- src/app/lib/action.js
+++ src/app/lib/action.js
@@ -16,6 +16,6 @@ export const addTemperature = async (data) => {
 
     }catch(err){
         console.log(err);
-        throw new Error ("Faailed ");
+        throw new Error ("faailed" + err);
     }
 }
