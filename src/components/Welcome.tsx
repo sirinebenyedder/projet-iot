7a359c355e8ff52cf1959d2c1d2c88483c2a6001
boubaker-abderrahman <boubakerabderrahman2003@gmail.com>
2024-12-16 09:02:24 +0100

    Addd ICon

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,15 @@
-import { CloudSun } from "@/weather_icons/Icons";
+"use client"
+import useWeatherStore from "@/store/weather.store";
+import { BigSun, BigSunCloud, CloudSun, IceIcon } from "@/weather_icons/Icons";
 
 export function Welcome() {
+    const {temperature} = useWeatherStore()
     return (
             <div className="flex m-4 gap-3 w-[500px]  justify-center items-center ">
-                {/* <IceIcon/> */}
-                <CloudSun/>
-                {/* <BigSunCloud/> */}
-                {/* <BigSun/> */}
+                {temperature > 40 && <BigSun/> }
+                {temperature < 40 && temperature > 20 && <BigSunCloud/>  }
+                {temperature >5 && temperature<20 && <CloudSun/> }
+                {temperature < 5 && <IceIcon/> }
             </div>
     )
 };
